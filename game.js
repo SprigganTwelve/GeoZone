320de971d53a5154e822b67abda13433fb43d988
Paterne <angepaterne.dali@gmail.com>
2024-04-18 16:48:59 +0200

    .

--- game.js
+++ game.js
@@ -1,51 +1,72 @@
 /*map is already add in an other js doc */
 
 let container = document.querySelector('.container')
+let popup = document.querySelector('.popup')
 let bool = true
-let coords = {}
+let coords = {pointA:{},pointB: {}}
 map.on('click',gameHandler)
-
+let scoreField;
+let scorePoint
+let message
 //Handler
 
 function gameHandler(event) {
-   console.log(event)
-    coords.pointA.lat = event.latlng.lat
-    coords.pointA.lng = event.latlng.lng
-   if(bool){
-    // var customIcon = L.icon({
-    //     // iconUrl: 'marker-icon.png', // URL de l'image de l'icône
-    //     iconSize: [25, 41],  // Taille de l'icône [largeur, hauteur]
-    //     iconAnchor: [12, 41], // Point d'ancrage de l'icône [horizontal, vertical]
-    //     popupAnchor: [1, -34], // Point d'ancrage du popup [point horizontal et vertical]
-    //     shadowUrl: 'marker-shadow.png', // URL de l'ombre de l'icône
-    //     shadowSize: [41, 41], // Taille de l'ombre de l'icône [largeur, hauteur]
-    //     shadowAnchor: [12, 41], // Point d'ancrage de l'ombre de l'icône
-    //     iconColor: 'red' // Couleur de l'icône (utilisation d'une icône en noir et blanc avec une couleur d'incrustation)
-    // });
-       L.marker([coords.pointA.lat,coords.pointA.lng ], {style: {fill : 'red'}}).addTo(map);
-   }
+    getCountries(true,event)
 }
+
+
+getCountries(false)
 
 function randomNumber (x) {
     let random = Math.floor(Math.random()*x+1)
     return random
 }
 
-async function getCountries() {
+async function getCountries(gameStarted,event = {}) {
     let url = "https://restcountries.com/v3.1/all"
-    fetch(url).then(resp => resp.json()).then(
+   await fetch(url).then(resp => resp.json()).then(
         (data)=>{
-           let random = randomNumber(data.length);
-           let targetedCountry = data[random]
-           console.log(targetedCountry)
-           let  divHtmlElemnt  = document.createElement('div')
-           let htmlScript = `
-           <span>${targetedCountry.name.common}</span>
-           <span>score : 0 </span>
-           `
-           divHtmlElemnt.classList.add('down')
-           divHtmlElemnt.innerHTML = htmlScript
-           container.appendChild(divHtmlElemnt)
+            if(gameStarted){
+                coords.pointA.lat = event.latlng.lat
+                coords.pointA.lng = event.latlng.lng 
+                console.log(coords)
+                if(bool){
+                   L.marker([coords.pointA.lat,coords.pointA.lng ], {style: {fill : 'red'}}).addTo(map);
+                   let segmentAB = haversing(coords)
+                   console.log(scoreField)
+                  switch (true) {
+                    case (segmentAB < 50):
+                        message = "<h2 class ='message'>Super Proche ! <h2>"
+                        break;
+                    case (segmentAB < 100):
+                            message = "<h2 class ='message'>Pas mal ! <h2>"
+                            break; 
+                    case (segmentAB < 500):
+                            message = "<h2 class ='message'>Mouais ! <h2>"
+                            break; 
+                    case (segmentAB < 800):
+                            message = "<h2 class ='message'> ça fait cher le TGV <h2>"
+                            break;
+                    case (segmentAB == 0):
+                                message = "<h2 class ='message'>vous être un monstre! <h2>"
+                                break;
+                    default:
+                        message = "<h2 class ='message'>Trop loin! <h2>"
+                        break;
+
+                  }
+                  popup.innerHTML = message
+                  popup.classList.add('animate','animate__bounceIn') // css animate
+                  let next = document.createElement('button')
+                  next.textContent = 'Next'
+                  next.classList.add('next')
+                  popup.appendChild(next)
+                  
+                }
+            }
+            else{
+                setRandomCountry(data,coords)
+            }
         }
     )
     .catch ((error)=>{
@@ -53,30 +74,67 @@ async function getCountries() {
     })
 }
 
-getCountries()
+
+
+
 
 function haversing(coords) {
 
-    Number.prototype.toRad = function() {
-        return this * Math.PI / 180;
-     }
-     
-     var lat2 = coords.pointB.lat; 
-     var lon2 = coords.pointB.lon; 
-     var lat1 = coords.pointA.lat; 
-     var lon1 = coords.pointA.lon; 
-     
-     var R = 6371; // km 
-     //has a problem with the .toRad() method below.
-     var x1 = lat2-lat1;
-     var dLat = x1.toRad();  
-     var x2 = lon2-lon1;
-     var dLon = x2.toRad();  
-     var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
-                     Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
-                     Math.sin(dLon/2) * Math.sin(dLon/2);  
-     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-     var d = R * c; 
-     
-     return d
+    const R = 6371; // en km
+    // On convertit les degrés en radian
+    const lat1 = coords.pointA.lat * Math.PI / 180;
+    const lat2 = coords.pointB.lat * Math.PI / 180;
+    const lon1 = coords.pointA.lng * Math.PI / 180;
+    console.log(lon1)
+    const lon2 = coords.pointB.lon * Math.PI / 180;
+    
+
+    // On calcule la distance entre les deux points
+    const dLat = lat2 - lat1;
+    const dLon = lon2 - lon1;
+  
+    // Formule de Haversine
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(lat1) * Math.cos(lat2) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  
+    const distance = R * c;
+    console.log(typeof(distance));
+    // console.log("R : "+ R + " ; Point A lat : "+ lat1 + " ,lon : "+ lon1 + " ; point B lat : "+ lat2 +" , lon: "+ lon2 +" ; dlat: "+ dLat +" ; dlon: "+ dLon +" ; a: "+ a +  " ; c :"+ c +" ; distance: "+ distance);
+    return distance
+
 }
+
+
+function setRandomCountry(data,coords) {
+    let random = randomNumber(data.length);
+    console.log(data)
+    let targetedCountry = data[random]
+    console.log(targetedCountry)
+    let  divHtmlElemnt  = document.createElement('div')
+    let htmlScript = `
+    <span>${targetedCountry.name.common}</span>
+    <span id='score'>score : 0 </span>
+    `
+    
+    divHtmlElemnt.classList.add('down')
+    divHtmlElemnt.innerHTML = htmlScript
+    scoreField = divHtmlElemnt.querySelector('#score')
+    container.appendChild(divHtmlElemnt)
+    coords.pointB.lat = targetedCountry.latlng[0]
+    coords.pointB.lon = targetedCountry.latlng[1]
+}
+
+//Bonus
+                // var customIcon = L.icon({
+                //     // iconUrl: 'marker-icon.png', // URL de l'image de l'icône
+                //     iconSize: [25, 41],  // Taille de l'icône [largeur, hauteur]
+                //     iconAnchor: [12, 41], // Point d'ancrage de l'icône [horizontal, vertical]
+                //     popupAnchor: [1, -34], // Point d'ancrage du popup [point horizontal et vertical]
+                //     shadowUrl: 'marker-shadow.png', // URL de l'ombre de l'icône
+                //     shadowSize: [41, 41], // Taille de l'ombre de l'icône [largeur, hauteur]
+                //     shadowAnchor: [12, 41], // Point d'ancrage de l'ombre de l'icône
+                //     iconColor: 'red' // Couleur de l'icône (utilisation d'une icône en noir et blanc avec une couleur d'incrustation)
+                // });
